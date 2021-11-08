998423b33ac758e90ac78cc4634d264fd5f9b037
Xavier Blanchot <xav.blanchot@gmail.com>
2021-11-08 17:29:07 +0100

    day 10 - P1

--- AOC2017/Xavier/10.py
+++ AOC2017/Xavier/10.py
@@ -1,5 +1,20 @@
 from sys import argv
 
+
+def rotate(l, n):
+    return l[n:] + l[:n]
+
+
+def twist(numbers, length, c_pos):
+    # 1. rotate to have c_pos at id 0
+    numbers = rotate(numbers, c_pos)
+    # 2. twist
+    numbers = numbers[:length][::-1] + numbers[length:]
+    # 3. rotate back
+    numbers = rotate(numbers, -c_pos)
+    return numbers
+
+
 if __name__ == '__main__':
 
     # Part 1
@@ -12,4 +27,11 @@ if __name__ == '__main__':
     c_pos = 0
     skip_size = 0
 
-    print(twists)
+    numbers = list(range(N))
+    for length in twists:
+        numbers = twist(numbers, length, c_pos)
+        c_pos = (c_pos + length + skip_size) % N
+        skip_size += 1
+        #print(numbers, " c_pos = ", c_pos, " skip = ", skip_size)
+
+    print("Part 1 ", numbers[0]*numbers[1])
