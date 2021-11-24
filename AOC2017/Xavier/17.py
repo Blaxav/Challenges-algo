3a89a5a8a768977bb8c12d9136480c624fa02ee9
Xavier Blanchot <xav.blanchot@gmail.com>
2021-11-24 01:09:56 +0100

    2017 - 17 and 18

--- AOC2017/Xavier/17.py
+++ AOC2017/Xavier/17.py
@@ -1,5 +1,39 @@
 from sys import argv
 
+
+def algo_list(N, searched_value, step):
+    buffer = [0]
+
+    pos = 0
+    for k in range(1, N):
+        #print((pos, step, k, (pos + step) % k), (pos + step) % k + 1)
+        id = (pos + step) % k + 1
+        buffer.insert(id, k)
+        pos = id
+        # print(buffer[k])
+        if k % 500000 == 0:
+            print(k)
+
+    return buffer[buffer.index(searched_value) + 1]
+
+
+def part2(N, step):
+    '''
+    The algorithm never insert before 0, then 0 stays at index 0 during all the process
+    Searching the value after 0 is equivalent to search the last value inserted at 
+    index 1, no buffer is needed
+    '''
+    pos = 0
+    result = -1
+    for k in range(1, N):
+        id = (pos + step) % k + 1
+        if id == 1:
+            result = k
+        pos = id
+
+    return result
+
+
 if __name__ == '__main__':
 
     if len(argv) < 2:
@@ -15,16 +49,6 @@ if __name__ == '__main__':
         buffer = buffer[:id] + [k] + buffer[id:]
         pos = id % len(buffer)
 
-    print("Part 1 : ", buffer[buffer.index(2017) + 1])
-
-    N = 50000000
-    buffer = [0]*N
-    pos = 0
-    for k in range(1, N + 1):
-        id = (pos + step) % k + 1
-        buffer = buffer[:id] + [k] + buffer[id:]
-        pos = id % k
-        if k % 1000 == 0:
-            print(k)
-
-    print("Part 2 : ", buffer[buffer.index(0) + 1])
+    print("Exemple : ", algo_list(2018, 2017, 3))
+    print("Part 1 : ", algo_list(2018, 2017, step))
+    print("Part 2 : ", part2(int(5e7+1), step))
