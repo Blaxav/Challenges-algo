16b9f1fbc74f2a28cf09a9ca0326567e4d1820d7
Xavier Blanchot <xav.blanchot@gmail.com>
2021-12-06 17:38:23 +0100

    2021 d3 + d1 redo

--- AOC2021/Xavier/3.py
+++ AOC2021/Xavier/3.py
@@ -9,6 +9,41 @@ def decimal(bits):
     return int(''.join([str(i) for i in bits]), 2)
 
 
+def filter_most_common(numbers, sub_ids, index, val):
+    '''
+    Filters a sublist of binary numbers, keeping only those with the most common value 
+    at a given index. In case of equality, val is used as most common
+    Returns the list of indices to keep in the list, the list itself is not modified
+    '''
+    keep_ids = []
+    zeros = [i for i in range(len(numbers)) if numbers[i]
+             [index] == 0 and i in sub_ids]
+    ones = [i for i in range(len(numbers)) if numbers[i]
+            [index] == 1 and i in sub_ids]
+
+    if (len(ones) > len(sub_ids) / 2) or (val == 1 and len(ones) == len(sub_ids) / 2):
+        return ones
+    elif len(ones) < len(sub_ids) / 2 or (val == 0 and len(ones) == len(sub_ids) / 2):
+        return zeros
+    else:
+        print("ERROR equality val not 0 or 1")
+
+
+def filter_least_common(numbers, sub_ids, index, val):
+    keep_ids = []
+    zeros = [i for i in range(len(numbers)) if numbers[i]
+             [index] == 0 and i in sub_ids]
+    ones = [i for i in range(len(numbers)) if numbers[i]
+            [index] == 1 and i in sub_ids]
+
+    if (len(ones) < len(sub_ids) / 2) or (val == 1 and len(ones) == len(sub_ids) / 2):
+        return ones
+    elif len(ones) > len(sub_ids) / 2 or (val == 0 and len(ones) == len(sub_ids) / 2):
+        return zeros
+    else:
+        print("ERROR equality val not 0 or 1")
+
+
 if __name__ == '__main__':
 
     bits = []
@@ -28,3 +63,18 @@ if __name__ == '__main__':
             gamma[j] = 1
 
     print("Part 1 : ", decimal(gamma) * decimal(compl(gamma)))
+
+    index = 0
+    oxygen_ids = [i for i in range(len(bits))]
+    while len(oxygen_ids) > 1:
+        oxygen_ids = filter_most_common(bits, oxygen_ids, index, 1)
+        index += 1
+
+    index = 0
+    co2_ids = [i for i in range(len(bits))]
+    while len(co2_ids) > 1:
+        co2_ids = filter_least_common(bits, co2_ids, index, 0)
+        index += 1
+
+    print("Part 2 : ", decimal(
+        bits[oxygen_ids[0]]) * decimal(bits[co2_ids[0]]))
