079a2b7a2ba03bb9be38202a5a702d9da1b4e832
Xavier Blanchot <xav.blanchot@gmail.com>
2021-12-07 18:32:59 +0100

    2021 Day 6 clean

--- AOC2021/Xavier/6.py
+++ AOC2021/Xavier/6.py
@@ -1,8 +1,13 @@
 from sys import argv
+from time import time
 
 if __name__ == '__main__':
 
-    N_steps = int(argv[2])
+    if len(argv) < 2:
+        print("Usage : python3 6.py file_path")
+        print("   ex : python3 6.py input/6.txt")
+        exit()
+    N_steps = 256
 
     input_data = open(argv[1], 'r')
     all_fishes = [int(i)
@@ -19,6 +24,7 @@ if __name__ == '__main__':
     # Waiting fishes will enter the cycle next iteration and sum up with a fish group
     waiting_fishes = 0
 
+    t0 = time()
     id = 0
     for t in range(N_steps):
         temp = waiting_fishes
@@ -27,9 +33,11 @@ if __name__ == '__main__':
         new_fishes = fishes[id]
         fishes[id] += temp
 
-        # print("After ", t+1, " days, total = ", sum(
-        #    fishes) + new_fishes + waiting_fishes)
+        if t == 79:
+            print("After 80  days, total = ", sum(
+                fishes) + new_fishes + waiting_fishes)
         id = (id + 1) % n_groups
 
-    print("After ", N_steps, " days, total = ",
+    print("After", N_steps, "days, total = ",
           sum(fishes) + new_fishes + waiting_fishes)
+    print("Time %-1.3es" % (time() - t0))
